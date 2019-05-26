3473954705f2e6b1da511d2123316d660511979b
witoopl <witoopl@gmail.com>
2019-05-26 13:51:34 +0200

    Page 121 (141 of 780). Shows number of products and categories

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,5 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
-})
-export class AppComponent {
-  title = 'app';
-}
+import { Component } from "@angular/core";
+@Component({  
+  selector: "app",  
+  template: `<store></store>`
+}) export class AppComponent { }
